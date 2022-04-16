7fa4f72add56020e8e50679ea4cbe7d026b5fb12
Dsemedo <dsemedoo@gmail.com>
2022-04-16 17:02:44 -0300

    Tentativas no JS

--- scripts.js
+++ scripts.js
@@ -1,16 +1,49 @@
-const perguntaa = prompt("Como deseja ser chamado?")
+const pergunta = document.querySelector(".pergunta");
+prompt(pergunta.innerHTML);	 
 
-const usuario = document.querySelector("pergunta").value;
-
-const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
-promessa.then(processarResposta);
-
-function processarResposta(resposta) {
-	console.log(resposta.data);
+function entrada(){ 
+const usuario = document.querySelector(".pergunta").value;
+const pessoa = {name: usuario};
+const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', pessoa);
+console.log(promessa);
+promessa.then(processarSucesso);
+promessa.catch(processarErro);
 }
 
-const manterConectado = axios.post('https://mock-api.driven.com.br/api/v6/uol/status')
+function processarSucesso(usuario) {
+	console.log(usuario.data);
+}
 
-const buscarMsg = axios.get('hhttps://mock-api.driven.com.br/api/v6/uol/messages')
+function processarErro(){
+	console.log(usuario.data);	
+}
 
-const enviarMsg = axios.post('hhttps://mock-api.driven.com.br/api/v6/uol/messages')
+function conectado(){ 
+	const pessoa = {name: usuario};
+	const manterConectado = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', pessoa);
+	console.log(manterConectado);
+	promessa.then(processarResposta);
+	promessa.catch(processarErro);
+	setInterval(4000, pessoa);
+}
+
+
+
+function erroInicio(){
+	if (error.response.status === 400){
+		alert("Usuario já ezistente, tente outro nome")
+	} 
+}
+
+
+const buscarMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+function tratarSucesso(resposta) {
+	console.log("Resposta recebida com sucesso!");
+}
+
+function tratarFalha(erro) {
+	console.log("Resposta recebida com falha!");
+}
+
+const enviarMsg = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages')
