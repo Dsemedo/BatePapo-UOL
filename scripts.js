1d12aa257eb724a0cba9e243c7a0f90aa19002fc
Dsemedo <dsemedoo@gmail.com>
2022-04-17 20:55:22 -0300

    Faltando aparecer as msgs

--- scripts.js
+++ scripts.js
@@ -4,25 +4,63 @@ function entrada(){
 let user = prompt("Como deseja ser chamado no Bate Papo mais famoso do mundo?");
 let login = {
 	name:user
-}
+	}
 let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', login);
 console.log(login);
 promessa.then(conectado);
 promessa.catch(entrada);
 }
+entrada();
 
 function conectado(){ 
 	let status = {name:user};
 	axios.post('https://mock-api.driven.com.br/api/v6/uol/status', status);
 	console.log(conectado);
-	setInterval(4000, status);
+	setInterval(3000, status);
+}
+conectado();
+
+function buscarMsgs(dados){
+	let receberMsg = dados.data;
+	const carregarMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages', receberMsg);
+	console.log(carregarMsg);
+	for (let i = 0; i < receberMsg.length; i++){
+		console.log("buscarMsgs()");		
+	const tiposMensagem = buscarMsgs[i].type;
+	// let chat = document.querySelector(".mensagem");
+	if(tiposMensagem === "status"){
+		chat.innerHTML += `<div class="status">
+		<span>${receberMsg[i].time}</span>
+		<span>${receberMsg[i].from}</span>
+		</div>`
+		}
+		else if(tiposMensagem === "message"){
+			chat.innerHTML += `<div class="paraTodos">
+			<span>${receberMsg[i].time}</span>
+			<span>${receberMsg[i].from}</span>
+			
+			</div>`
+			}
+		else if(tiposMensagem === "private_message"){
+				chat.innerHTML += `<div class="reservadas">
+				<span>${receberMsg[i].time}</span>
+				<span>${receberMsg[i].from}</span>
+				</div>`
+			}
+	}
+
+
 }
 
-
+// function apagarMsg(){
+// 	const msgInicial = document.querySelector(".escreva");
+// 	msgInicial.innerHtml = " ";
+// }
+// apagarMsg();
 
 // function erroInicio(){
 // 	if (error.response.status === 400){
-// 		alert("Usuario já ezistente, tente outro nome")
+// 		alert("Usuario já eistente, tente outro nome")
 // 	} 
 // }
 
@@ -38,3 +76,8 @@ function conectado(){
 // }
 
 // const enviarMsg = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages')
+
+
+
+
+
